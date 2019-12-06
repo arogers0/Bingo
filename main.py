26bd98da5406e26c2ba0c413c0bb007a12d173cf
arogers0 <58483124+arogers0@users.noreply.github.com>
2019-12-06 11:52:57 -0500

    Minor changes
    Removed unnecessary comments. Centered boards.

--- main.py
+++ main.py
@@ -19,31 +19,14 @@ board1 = Board(root, 1)
 
 boardFrame = Frame(root)
 
-#board3a = Board(boardFrame, 3, board3.tiles)
-#board2a = Board(boardFrame, 2, board2.tiles)
-#board1a = Board(boardFrame, 1, board1.tiles)
-
-#board3a.boardFrame.pack(side=RIGHT)
-#board2a.boardFrame.pack(side=RIGHT)
-#board1a.boardFrame.pack(side=RIGHT)
-#boardFrame.place(relx=0.5, rely=0.2, anchor=N)
-#boardFrame.lift()
-#print(board3a.tiles[0][0].value)
-
 boardsList = [board1, board2, board3]
-
-#buttonFrame = Frame(root)
-#boardButton1 = Button(buttonFrame, text="Board 1", command=board1.focus).pack(side=LEFT)
-#boardButton2 = Button(buttonFrame, text="Board 2", command=board2.focus).pack(side=LEFT)
-#boardButton3 = Button(buttonFrame, text="Board 3", command=board3.focus).pack(side=LEFT)
-#buttonFrame.pack()
 
 board1.render()
 board2.render()
 board3.render()
-board1.boardFrame.pack(side=LEFT)
-board2.boardFrame.pack(side=LEFT)
-board3.boardFrame.pack(side=LEFT)
+board1.boardFrame.pack(side=LEFT, expand=YES)
+board2.boardFrame.pack(side=LEFT, expand=YES)
+board3.boardFrame.pack(side=LEFT, expand=YES)
 
 gameButtonFrame = Frame(root)
 singleIterButton = Button(gameButtonFrame, text="Call A Value", command=lambda: Utility.singleIteration(boardsList)).pack(side=LEFT)
